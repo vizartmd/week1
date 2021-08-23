96e6d52a116ebfafb8941c839471a8289fdc1424
vizartmd <vizart.md@gmail.com>
2021-08-23 18:33:18 +0300

    Update DAOFactory.java

--- wrapper/servicedao/src/main/java/com/servicedao/dao/DAOFactory.java
+++ wrapper/servicedao/src/main/java/com/servicedao/dao/DAOFactory.java
@@ -5,24 +5,29 @@ import java.sql.SQLException;
 import com.servicedao.daointf.TaskDAO;
 import com.servicedao.daointf.UserDAO;
 
+/**
+ * This class provides a factory from many different types of DAOFactory
+ * "Abstract Factory"-Pattern
+ */
 public abstract class DAOFactory {
 
 	public static final int MYSQL = 1;
 	public static final int ORACLE = 2;
 
 	public abstract UserDAO getUserDAO() throws SQLException;
+
 	public abstract TaskDAO getTaskDAO() throws SQLException;
-	
+
 	public static DAOFactory getDAOFactory(int whichFactory) {
-		
-		switch(whichFactory) {
+
+		switch (whichFactory) {
 		case MYSQL:
 			return new MySQLDAOFactory();
 		case ORACLE:
 			return new OracleDAOFactory();
 		}
 		return null;
-		
+
 	}
 
 }
