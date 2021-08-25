9401b496a8bb834075f280f850b96b8d8a7e4006
vizartmd <vizart.md@gmail.com>
2021-08-25 15:12:57 +0300

    hibernation runs without exception, but the tables are not created, I put the project on hold and move on to the task for week 1 after reviewing it with my mentor

--- main/src/main/java/com/main/App.java
+++ main/src/main/java/com/main/App.java
@@ -1,20 +1,47 @@
 package com.main;
 
-import java.sql.SQLException;
-
 import org.apache.log4j.BasicConfigurator;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import com.servicedao.domain.User;
 import com.servicedao.hibernate.HibernateUtil;
 
 public class App {
 	public static void main(String[] args) {
 
-		BasicConfigurator.configure();
-		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-		Session session = sessionFactory.openSession();
-		
+		SomeClass someClass = new SomeClass();
+		Class clss = someClass.getClass();
+		SomeClass someClass1 = (SomeClass)clss.newInstance();
 	}
-} 
+}
+
+class SomeClass {
+	private int age;
+	private String name = "Vasea";
+
+	public SomeClass() {
+	}
+
+	public SomeClass(int age, String name) {
+		super();
+		this.age = age;
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
--- servicedao/src/main/java/com/servicedao/hibernate/HibernateUtil.java
+++ servicedao/src/main/java/com/servicedao/hibernate/HibernateUtil.java
@@ -1,30 +1,79 @@
 package com.servicedao.hibernate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
+import org.hibernate.cfg.Environment;
 
+import com.fasterxml.classmate.AnnotationConfiguration;
 public class HibernateUtil {
-	private static SessionFactory sessionFactory;
-    private static ServiceRegistry serviceRegistry;
+	
+	private static StandardServiceRegistry registry;
+	   private static SessionFactory sessionFactory;
 
-    private static SessionFactory createSessionFactory() {
-        if (sessionFactory == null) {
-            // loads configuration and mappings
-            Configuration configuration = new Configuration().configure();
-            configuration.configure();
-            serviceRegistry
-                = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);        
-        }
-		return sessionFactory;
-    }
-        
-        public static SessionFactory getSessionFactory() {
-            if (sessionFactory == null)
-                sessionFactory = createSessionFactory();
-            return sessionFactory;
-        }
+	   public static SessionFactory getSessionFactory() {
+	      if (sessionFactory == null) {
+	         try {
+
+	            // Create registry builder
+	            StandardServiceRegistryBuilder registryBuilder = new StandardServiceRegistryBuilder();
+
+	            // Hibernate settings equivalent to hibernate.cfg.xml's properties
+	            Map<String, String> settings = new HashMap<>();
+	            settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+	            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testdb");
+	            settings.put(Environment.USER, "root");
+	            settings.put(Environment.PASS, "RoOt_aDmIn");
+	            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+
+	            // Apply settings
+	            registryBuilder.applySettings(settings);
+
+	            // Create registry
+	            registry = registryBuilder.build();
+
+	            // Create MetadataSources
+	            MetadataSources sources = new MetadataSources(registry);
+
+	            // Create Metadata
+	            Metadata metadata = sources.getMetadataBuilder().build();
+
+	            // Create SessionFactory
+	            sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+	         } catch (Exception e) {
+	            e.printStackTrace();
+	            if (registry != null) {
+	               StandardServiceRegistryBuilder.destroy(registry);
+	            }
+	         }
+	      }
+	      return sessionFactory;
+	   }
+
+	   public static void shutdown() {
+	      if (registry != null) {
+	         StandardServiceRegistryBuilder.destroy(registry);
+	      }
+	   }
+	
+//	private static SessionFactory sessionFactory;
+//	
+//	static {
+//		Configuration cfg = new Configuration().configure();
+//		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+//				.applySettings(cfg.getProperties());
+//		
+//		sessionFactory = cfg.buildSessionFactory(builder.build());
+//	}
+//	
+//	public static SessionFactory getSessionFactory() {
+//		return sessionFactory;
+//	}
 }
