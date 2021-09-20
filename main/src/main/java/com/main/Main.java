8f97157d9d96063ff84046f0e078767eb362e860
vizartmd <vizart.md@gmail.com>
2021-09-20 22:42:19 +0300

    testing newFixedThreadPool(1) from Main class

--- main/src/main/java/com/main/Main.java
+++ main/src/main/java/com/main/Main.java
@@ -1,13 +1,19 @@
 package com.main;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import org.apache.log4j.BasicConfigurator;
+import com.main.command.CommandInvoker;
+import com.main.command.usercommand.UserGetAllCommand;
 import com.main.multithreading.ThreadAssignTaskToUser;
 import com.main.multithreading.ThreadCreateUser;
+import com.main.multithreading.ThreadShowAllTasks;
+import com.main.multithreading.ThreadShowAllUsers;
 import com.servicedao.domain.Task;
 import com.servicedao.domain.User;
 
@@ -41,24 +47,16 @@ public class Main {
 		BasicConfigurator.configure();
 		Main main = new Main();
 		main.collectInfo();
-		ExecutorService executorService = Executors.newFixedThreadPool(4);
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
 		ThreadCreateUser threadCreateUser = new ThreadCreateUser(main.user);
-		Future createUser = executorService.submit(threadCreateUser);
-		createUser.get();
+		executorService.submit(threadCreateUser);
 		ThreadAssignTaskToUser threadAssignTaskToUser = new ThreadAssignTaskToUser(main.task, main.user.getUserName());
-		Future createTask = executorService.submit(threadAssignTaskToUser);
-		createTask.get();
-//		List<Callable<Void>> list = new ArrayList<>();
-//		list.add(new ThreadShowAllUsers().newCallable());
-//		list.add(new ThreadShowAllTasks().newCallable());
-//		executorService.invokeAll(list);
+		 executorService.submit(threadAssignTaskToUser);
+		List<User> myUsers = (List<User>) executorService.submit(new ThreadShowAllUsers().newCallable()).get();
+		List<Task> myTasks = (List<Task>) executorService.submit(new ThreadShowAllTasks().newCallable()).get();
+		System.out.println(myUsers);
+		System.out.println(myTasks);
 		executorService.shutdown();
-
-//		List<User> userList = (List<User>) ((Future) new ThreadShowAllUsers().newCallable()).get();
-//		List<Task>tasksList = (List<Task>) ((Future) new ThreadShowAllTasks().newCallable()).get();
-//		System.out.println(userList);
-//		System.out.println(tasksList);
-		
 		System.out.println("Finished");
 	}
 }
