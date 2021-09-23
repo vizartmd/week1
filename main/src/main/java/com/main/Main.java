b63c70abea134057a5d32901fe1cb1d58d6dbf55
vizartmd <vizart.md@gmail.com>
2021-09-23 16:49:10 +0300

    elimination of multiples try/catch from Main class

--- main/src/main/java/com/main/Main.java
+++ main/src/main/java/com/main/Main.java
@@ -10,6 +10,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+
 import com.main.multithreading.ThreadAssignTasksToUser;
 import com.main.multithreading.ThreadCreateUser;
 import com.main.multithreading.ThreadShowAllUsers;
@@ -24,34 +26,38 @@ public class Main {
 
 		BasicConfigurator.configure();
 		Logger logger = Logger.getLogger(Main.class);
-		logger.info("Starting");
 
+		logger.info("Starting");
 		ExecutorService executorService = Executors.newFixedThreadPool(4);
 
 		User user = CollectInfo.createUser();
 		Set<Task> tasks = CollectInfo.createTask();
-		ThreadCreateUser threadCreateUser = new ThreadCreateUser(user);
-		executorService.submit(threadCreateUser).get();
+		try {
+			ThreadCreateUser threadCreateUser = new ThreadCreateUser(user);
+			executorService.submit(threadCreateUser).get();
 
-		ThreadAssignTasksToUser threadAssignTasksToUser = new ThreadAssignTasksToUser(tasks, user.getUserName());
+			ThreadAssignTasksToUser threadAssignTasksToUser = new ThreadAssignTasksToUser(tasks, user.getUserName());
+			executorService.submit(threadAssignTasksToUser).get();
 
-		executorService.submit(threadAssignTasksToUser).get();
-		Set<User> myUsers = null;
-		myUsers = (Set<User>) executorService.submit(new ThreadShowAllUsers().newCallable()).get();
-		List<Integer> usersId = CollectInfo.getUsersIds(myUsers);
-		System.out.println("All user's ID: " + usersId);
+			Set<User> myUsers = null;
+			myUsers = (Set<User>) executorService.submit(new ThreadShowAllUsers().newCallable()).get();
+			List<Integer> usersId = CollectInfo.getUsersIds(myUsers);
+			System.out.println("All user's ID: " + usersId);
 
-		Set<Task> myTasks = null;
-		Integer userId = 0;
-		userId = CollectInfo.selectUserIdFromExistingUsers();
-		myTasks = (Set<Task>) executorService.submit(new ThreadShowUsersTasksByUserId(userId).newCallable()).get();
-
-		System.out.println("Tasks of user by ID: " + userId + " -> " + myTasks);
-		executorService.shutdown();
-		if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+			Set<Task> myTasks = null;
+			Integer userId = 0;
+			userId = CollectInfo.selectUserIdFromExistingUsers();
+			myTasks = (Set<Task>) executorService.submit(new ThreadShowUsersTasksByUserId(userId).newCallable()).get();
+			System.out.println("Tasks of user by ID: " + userId + " -> " + myTasks);
 			executorService.shutdown();
+			if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+				executorService.shutdown();
+			}
+		} catch (IllegalStateException | HibernateException | InterruptedException | ExecutionException e) {
+			e.printStackTrace();
 		}
 
 		System.out.println("Finished");
+
 	}
 }
